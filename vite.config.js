5558f8e2e718289b551f7418f48f9aef70176215
AgilS121 <gilsatria121@gmail.com>
2023-12-12 05:59:10 +0700

    test

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react()],
   build: {
     assetsDir: 'assets', // Adjust the assets directory based on your project structure
     // Uncomment the next block if you still encounter issues after trying the suggestions
     // rollupOptions: {
-    //   external: ['@fortawesome/fontawesome-svg-core', 'react-data-table-component'],
+    //   external: ['react-data-table-component'],
     // },
     rollupOptions: {
-      external: ['styled-components'],
+      external: ['/styled-components'],
     },
   },
 });
